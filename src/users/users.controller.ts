dbe82fda380369832167f33fddca5bddeac34907
Carlos Nava <yeguacelestial@gmail.com>
2024-04-04 20:06:05 -0600

    create users controller, update tasks module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './models/user.entity';
 
 @Controller('api/users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Post()
+  @HttpCode(201)
+  async create(@Body() user: User): Promise<User> {
+    const createdUser = await this.usersService.create(user);
+    return createdUser;
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() user: User): Promise<any> {
+    await this.usersService.update(id, user);
+    return { message: 'User updated successfully' };
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<any> {
+    const user = await this.usersService.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('User does not exist!');
+    }
+
+    await this.usersService.delete(id);
+    return { message: 'user deleted succesffuly' };
+  }
+}
